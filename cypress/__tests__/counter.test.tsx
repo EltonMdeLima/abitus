fec73dbe870faad807030048649b720d51191272
Elton M. Lima <elton.lima@sou.ufmt.br>
2025-09-09 21:31:45 -0400

    chore: Add 'test' script and Jest configuration

--- cypress/__tests__/counter.test.tsx
+++ cypress/__tests__/counter.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { Counter } from '@/app/counter';
 import { Provider } from 'react-redux';
 import { store } from '@/store';
+import React from 'react';
 
 // Função personalizada de renderização com Redux Provider
 const renderWithRedux = (component: React.ReactElement) => {
@@ -14,6 +15,10 @@ describe('Counter Component', () => {
   it('deve renderizar o contador com valor inicial 0', () => {
     // Usa a função personalizada para renderizar o componente
     renderWithRedux(<Counter />);
-
+    
+    // Procura o elemento que contém o valor do contador e verifica se é '0'
+    const countElement = screen.getByText('0');
+    expect(countElement).toBeInTheDocument();
   });
 });
+import '@testing-library/jest-dom/extend-expect';
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-};
+      // O ambiente de testes deve ser jsdom para testar componentes React
+      testEnvironment: 'jest-environment-jsdom',
+
+      // Ignora pastas que não contêm testes, como a do Cypress
+      testPathIgnorePatterns: [
+        '/node_modules/',
+        '/.next/',
+        '/cypress/',
+        '/.history/'
+      ],
+
+      // Configuração para o Jest usar o ts-jest para arquivos .ts(x)
+      preset: 'ts-jest',
+
+      // Configuração para importar o setup de testes
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    };
